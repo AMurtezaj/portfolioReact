99c9ea735a453235f891d2c63596b493e60adaf2
Altin Murtezaj <164541400+AMurtezaj@users.noreply.github.com>
2025-01-05 01:27:17 +0100

    changes

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -129,6 +129,42 @@ export default function MyPortfolio() {
             </a>
           </div>
         </div>
+        <div className="portfolio--section--card">
+          <div className="portfolio--section--img">
+            <img src="./img/nancys.png" alt="App Dashboard" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">Nancy's Restaurant</h3>
+              <p className="text-md">
+              This is a dynamic and user-friendly website developed for Nancy's Restaurant, designed to showcase its menu, ambiance, and services. The website provides an elegant interface for customers to explore dishes, and learn more about the restaurant’s story and special offerings. It features responsive design for seamless access across all devices and ensures an enjoyable browsing experience for food lovers.
+              </p>
+            </div>
+            <a
+              href="https://www.nancysrestaurant.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm portfolio--link"
+            >
+              View on the web
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 20 19"
+                fill="none"
+              >
+                <path
+                  d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                  stroke="currentColor"
+                  strokeWidth="2.66667"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
